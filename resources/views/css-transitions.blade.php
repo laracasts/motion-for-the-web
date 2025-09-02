8519642e7e9f0963df8618f13c76914c48ec78fc
Simon Vrachliotis <simon@simonswiss.com>
2025-09-02 10:11:29 +1000

    initial commit

---
+++ resources/views/css-transitions.blade.php
@@ -0,0 +1,6 @@
+<x-layout stylesheet="css-transitions.css">
+    <div class="wrapper">
+        <div class="square"></div>
+        <p>Hello!</p>
+    </div>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'intro');
+Route::view('/css-transitions', 'css-transitions');

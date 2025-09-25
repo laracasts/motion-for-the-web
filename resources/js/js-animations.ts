c141289bdb91e8d0212afbd46a2331677613c7fe
Simon Vrachliotis <simon@simonswiss.com>
2025-09-25 12:53:23 +1000

    Responding to User Interactions

--- resources/js/js-animations.ts
+++ resources/js/js-animations.ts
@@ -1,17 +1,54 @@
 const square = document.querySelector(".square") as HTMLDivElement;
+const playButton = document.querySelector("#play") as HTMLButtonElement;
+const pauseButton = document.querySelector("#pause") as HTMLButtonElement;
+const reverseButton = document.querySelector("#reverse") as HTMLButtonElement;
+const slider = document.querySelector("#slider") as HTMLInputElement;
 
 const keyframes = [
     { transform: "translateX(0) translateY(0)", easing: "ease-in-out" },
-    { transform: "translateX(100px)" },
-    { transform: "translateX(100px) translateY(100px)", easing: "steps(3)" },
-    { transform: "translateX(0) translateY(100px)" },
-    { transform: "translateX(0) translateY(0)" },
+    {
+        transform: "translateX(100px) rotate(45deg)",
+        offset: 0.2,
+        easing: "ease-in-out",
+    },
+    {
+        transform: "translateX(100px) translateY(100px) scale(0.8)",
+        easing: "ease-in-out",
+    },
+    {
+        transform: "translateX(0) translateY(100px) rotate(-90deg)",
+        offset: 0.8,
+        easing: "ease-in-out",
+    },
+    {
+        transform: "translateX(0) translateY(0) rotate(0deg)",
+        easing: "ease-in-out",
+    },
 ];
 
 const options: KeyframeAnimationOptions = {
-    duration: 5000,
+    duration: 3000,
     iterations: Infinity,
-    easing: "ease-in-out",
 };
 
-square.animate(keyframes, options);
+const animation: Animation = square.animate(keyframes, options);
+playButton.addEventListener("click", () => {
+    animation.play();
+});
+
+pauseButton.addEventListener("click", () => {
+    animation.pause();
+});
+reverseButton.addEventListener("click", () => {
+    animation.reverse();
+});
+slider.addEventListener("input", () => {
+    // Sync the slider value to the animation speed.
+    // animation.playbackRate = slider.valueAsNumber;
+
+    // Sync the animation progress to the slider value.
+    const totalDuration = animation.effect?.getTiming().duration as number;
+    console.log({ totalDuration });
+
+    animation.currentTime = slider.valueAsNumber * totalDuration;
+});
--- resources/views/js-animations.blade.php
+++ resources/views/js-animations.blade.php
@@ -1,3 +1,10 @@
 <x-layout title="Animation Loops in JS" stylesheet="js-animations.css" script="js-animations.ts">
  <div class="square"></div>
+
+ <div class="controls">
+  <button id="pause">Pause</button>
+  <button id="play">Play</button>
+  <button id="reverse">Reverse</button>
+  <input id="slider" type="range" value="0" min="0" max="1" step="0.01">
+ </div>
 </x-layout>

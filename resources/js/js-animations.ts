08ec52a56de80a51736f931623b53e4452a769a9
Simon Vrachliotis <simon@simonswiss.com>
2025-09-25 10:50:58 +1000

    RequestAnimationFrame

--- resources/js/js-animations.ts
+++ resources/js/js-animations.ts
@@ -1,11 +1,22 @@
 const square = document.querySelector(".square") as HTMLDivElement;
 
 let rotation = 0;
-const MAX_ROTATION = 45;
+const MAX_ROTATION = 360;
 
-const intervalId = setInterval(() => {
+function rotateSquare() {
     rotation++;
     square.style.transform = `rotate(${rotation}deg)`;
 
-    if (rotation >= MAX_ROTATION) clearInterval(intervalId);
-}, 1000 / 10); // 16.66666
+    if (rotation < MAX_ROTATION) {
+        requestAnimationFrame(rotateSquare);
+    }
+}
+
+rotateSquare();
+
+// const intervalId = setInterval(() => {
+//     rotation++;
+//     square.style.transform = `rotate(${rotation}deg)`;
+
+//     if (rotation >= MAX_ROTATION) clearInterval(intervalId);
+// }, 1000 / 10); // 16.66666

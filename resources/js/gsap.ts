98191ce1636a323fc2ef0438cdfc46d103c9a701
Simon Vrachliotis <simon@simonswiss.com>
2025-10-28 20:56:33 +1100

    ScrollTrigger

--- resources/js/gsap.ts
+++ resources/js/gsap.ts
@@ -1,7 +1,19 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const tl = gsap.timeline();
+gsap.registerPlugin(ScrollTrigger);
 
-tl.to(".square", { x: 100, duration: 3 });
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".square",
+        markers: true,
+        start: "top bottom-=200",
+        end: "75% 200",
+        // toggleActions: "play reverse play reverse",
+        scrub: 0.5,
+    },
+});
+
+tl.to(".square", { x: 100 });
 tl.to(".square", { scale: 0.5 });
 tl.to(".square", { scale: 1, x: 200 });
